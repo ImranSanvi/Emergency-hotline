2a306a96dad09ca9fed1ee8bd71ea00e37905b90
ImranSanvi <imranmohammed5220@gmail.com>
2025-09-20 15:20:13 +0600

    copy functionality

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 
+callHistory = [];
 
 function numberOfHeart(){
     const available = document.getElementById('hear-count');
@@ -16,6 +17,18 @@ function numberOfCopy(){
     return availableConverted;
 }
 
+function coinChange(){
+    const available = document.getElementById('coin-count');
+    let availableConverted = parseInt(available.innerText);
+    if(availableConverted < 20){
+        alert('Not enough coin');
+        return false;
+    }
+    availableConverted -= 20;
+    available.innerText = availableConverted;
+    return true;
+}
+
 // Heart Button functionality
 const hearts = document.getElementsByClassName("heart");
 for (let heart of hearts){
@@ -30,6 +43,27 @@ const copyBtn = document.getElementsByClassName("copy-btn");
 for(let copy of copyBtn){
     copy.addEventListener('click', function(){
         numberOfCopy();
+        const card = copy.parentElement.parentElement;
+        const serviceNumber = card.querySelector(".service-number").innerText;
+        navigator.clipboard.writeText(serviceNumber).then(() => {
+            alert("Copied Successfully: " + serviceNumber);
+        })
+    })
+}
+
+
+// call Button clicked
+const calls = document.getElementsByClassName("call-btn");
+for(let call of calls){
+    call.addEventListener('click', function(){
+        const b = coinChange();
+        if(b){
+            const card = call.parentElement.parentElement;
+            const serviceName = card.querySelector(".service-name").innerText;
+            const serviceNumber = card.querySelector(".service-number").innerText;
+
+            alert(serviceName + "\n" + serviceNumber);
+        }
     })
 }
 
