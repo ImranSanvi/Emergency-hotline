306d91b2562c3d11b2a1c6af79ac5a5f4d3f4816
ImranSanvi <imranmohammed5220@gmail.com>
2025-09-20 13:02:51 +0600

    card update

--- script.js
+++ script.js
@@ -7,12 +7,30 @@ function numberOfHeart(){
     available.innerText = availableConverted;
     return availableConverted;
 }
-const hearts = document.getElementsByClassName("heart");
 
-for (let heart of hearts) {
+function numberOfCopy(){
+    const available = document.getElementById("copy");
+    let availableConverted = parseInt(available.innerText);
+    availableConverted += 1;
+    available.innerText = availableConverted;
+    return availableConverted;
+}
+
+// Heart Button functionality
+const hearts = document.getElementsByClassName("heart");
+for (let heart of hearts){
   heart.addEventListener("click", function () {
     numberOfHeart();
   });
+}
+
+
+// Copy Button Functionality
+const copyBtn = document.getElementsByClassName("copy-btn");
+for(let copy of copyBtn){
+    copy.addEventListener('click', function(){
+        numberOfCopy();
+    })
 }
 
 
